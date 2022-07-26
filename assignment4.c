f6be7d92422975114ed1f53f88e6116ec863ae26
CHETAN-SING <109669059+CHETAN-SING@users.noreply.github.com>
2022-07-26 13:22:52 +0530

    Add files via upload

---
+++ assignment4.c
@@ -0,0 +1,173 @@
+//1. Write a program to print MySirG 5 times on the screen.
+
+/*#include<stdio.h>
+ int main ()
+ {  int i =0;
+
+
+ while( i<=5)
+ {
+    printf("MySirG\n");
+    
+    i++;
+ }
+    return 0;
+ }
+ */
+
+//2. Write a program to print the first 10 natural numbers.
+
+/*#include<stdio.h>
+int main()
+{   int i=1;
+
+
+  while(i<=10)
+  {
+ printf(" %d", i);
+
+i++;
+}
+
+    return 0;
+}
+*/
+
+
+//3. Write a program to print the first 10 natural numbers in reverse order
+
+ /*#include<stdio.h>
+
+int main()
+{ int i=10;
+   
+    while(i>=1)
+
+
+   printf("%d ",i--);
+
+
+
+
+   return 0;
+}
+*/
+//4. Write a program to print the first 10 odd natural numbers
+
+
+/*#include<stdio.h>
+
+int main()
+{
+	printf("The First 10 Odd Natural Numbers are\n");
+
+	for (int i = 1; i <= 10; i++)
+	{
+		printf("%d\n", 2 * i - 1);
+	}
+   return 0;
+}
+*/
+
+
+//5. Write a program to print the first 10 odd natural numbers in reverse order.
+
+
+/*#include<stdio.h>
+
+int main()
+
+{
+   printf("reverse order of first 10 odd  natural numbers\n");
+
+  for(int i =19; i>=1;i--)
+  {
+     printf("%d\n",i--);
+
+
+  }
+return 0;
+
+}
+*/
+
+//6. Write a program to print the first 10 even natural numbers
+ /*#include<stdio.h>
+
+ int main()
+ {
+     for ( int i=2; i<=20; i++)
+     {
+       printf("%d ",i++);
+
+     }
+
+
+
+   return 0;
+ }
+ */
+//7. Write a program to print the first 10 even natural numbers in reverse order
+
+/*#include<stdio.h>
+
+int main()
+{   
+   for(int i=20; i>=1;i--)
+   {
+      printf("%d ",i--);
+   }
+
+
+
+   return 0;
+}
+*/
+
+//8. Write a program to print squares of the first 10 natural numbers
+/*#include<stdio.h>
+
+int main()
+{
+      for(int i=1; i<=10; i++)
+{
+   printf("%d ",i*i);
+}
+
+   
+   return 0;
+}
+*/
+
+//9. Write a program to print cubes of the first 10 natural numbers
+/*#include<stdio.h>
+
+int main()
+{
+      for(int i=1; i<=10; i++)
+{
+   printf("%d ",i*i*i);
+}
+
+   
+   return 0;
+}
+*/
+
+//10. Write a program to print a table of 5
+
+/*#include<stdio.h>
+ 
+   int main()
+   { 
+      for(int i =1; i<=10; i++)
+      {
+         printf("%d ",5*i);
+      }
+  
+
+return 0;
+   }
+*/
+
+
